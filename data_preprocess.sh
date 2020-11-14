3f02ea47a67ecd53095ad59f854b63f108d8f100
zyzisyz <zyziszy@foxmail.com>
2020-11-14 16:47:52 +0800

    update

--- data_preprocess.sh
+++ data_preprocess.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-echo format data dir
-python -u scripts/format_data_dir.py
+stage=1
+echo This is stage: $stage
 
-echo downsample and upsample
-bash ./scripts/down_up_sample.sh
+if [ stage -eq 1 ];then
+	echo format data dir
+	python -u scripts/format_data_dir.py
+fi
 
-echo make MFCCs
+if [ stage -eq 2 ];then
+	echo downsample and upsample aug
+	bash ./scripts/down_up_sample.sh
+fi
+
+if [ stage -eq 3 ];then
+	echo griff lim aug
+fi
+
+if [ stage -eq 4 ];then
+	echo make MFCCs
+fi
+
