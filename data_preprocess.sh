14482a498d58b15137fa77a5806019e1aa5b8dc6
zyzisyz <zyziszy@foxmail.com>
2020-11-14 16:54:04 +0800

    update

--- data_preprocess.sh
+++ data_preprocess.sh
@@ -3,21 +3,22 @@
 stage=1
 echo This is stage: $stage
 
-if [ stage -eq 1 ];then
+if [ $stage -eq 1 ];then
 	echo format data dir
+	rm -rf data
 	python -u scripts/format_data_dir.py
 fi
 
-if [ stage -eq 2 ];then
+if [ $stage -eq 2 ];then
 	echo downsample and upsample aug
 	bash ./scripts/down_up_sample.sh
 fi
 
-if [ stage -eq 3 ];then
+if [ $stage -eq 3 ];then
 	echo griff lim aug
 fi
 
-if [ stage -eq 4 ];then
+if [ $stage -eq 4 ];then
 	echo make MFCCs
 fi
 
--- scripts/format_data_dir.py
+++ scripts/format_data_dir.py
@@ -9,6 +9,7 @@ from sklearn.model_selection import train_test_split
 import torch
 import os
 import shutil
+import time
 
 
 def load_file(csv_path="meta/esc50.csv", audio_dir="audio/"):
@@ -20,7 +21,7 @@ def load_file(csv_path="meta/esc50.csv", audio_dir="audio/"):
 
 if __name__ == "__main__":
 	file_path, labels = load_file()
-	train_path, test_path, train_label, test_label = train_test_split(file_path, labels, test_size=0.2, random_state=42, shuffle=True)
+	train_path, test_path, train_label, test_label = train_test_split(file_path, labels, test_size=0.2, random_state=int(time.time()), shuffle=True)
 	for label in np.unique(labels):
 		if not os.path.exists("./data/train/{}".format(label)):
 			os.makedirs("./data/train/{}".format(label))
